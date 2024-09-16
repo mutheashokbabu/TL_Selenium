d534af2127c842e7f4a45283cfff8a6fdc17df48
mutheashokbabu <102396591+mutheashokbabu@users.noreply.github.com>
2024-09-16 20:44:54 +0530

    Add files via upload

---
+++ EdgeBrowser.java
@@ -0,0 +1,13 @@
+package week1.day2;
+
+public class EdgeBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Browser browser = new Browser();
+		System.out.println(browser.launchBrowser("Edge"));
+		browser.loadUrl();
+	}
+
+}
